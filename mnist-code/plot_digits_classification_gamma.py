13c2e094b7dd69ac5add0ad1948eb12b0877ed3f
ayan-cs <ayanabhag@gmail.com>
2021-09-18 19:52:14 +0530

    different image and gamma values

--- mnist-code/plot_digits_classification_gamma.py
+++ mnist-code/plot_digits_classification_gamma.py
@@ -5,25 +5,33 @@ import math
 from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from skimage.transform import resize
 
-digits = datasets.load_digits()
-n_samples = len(digits.images)
+digit = datasets.load_digits()
+n_samples = len(digit.images)
 
-shapes=[16, 64, 256, 1024, 4096]
+target = digit.target
+digits = digit.images
+
+shapes=[8, 16, 64]
 data=[]
 for i in shapes:
-  temp = list(np.resize(digits.images, (n_samples, i)))
-  data.append(np.array(temp))
+    temp=[]
+    for img in digits:
+        temp.append(np.ravel(resize(img, (i, i), anti_aliasing=False)))
+    data.append(np.array(temp))
+    temp.clear()
 
 split=[0.1, 0.2, 0.3, 0.4]
 gamma = [0.1, 0.01, 0.001, 0.0001]
+#gamma = [0.0001]
 for g in gamma :
   print(f"\n=== Gamma = {g} ===\n")
   print("ImgSize\tTrn:Tst\tValAcc\tTstAcc")
   for d in data :
     for size in split :
       clf = svm.SVC(gamma=g)
-      X_train, X_test, y_train, y_test = train_test_split(d, digits.target, test_size=size, shuffle=False)
+      X_train, X_test, y_train, y_test = train_test_split(d, target, test_size=size, shuffle=False)
       X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=int(len(y_test)/2), shuffle=False)
       clf.fit(X_train, y_train)
       y_val_pred = clf.predict(X_val)
