64f1c2bc99081bef433acc18df575c8dbf686da1
AyanG <87515557+ayan-cs@users.noreply.github.com>
2021-10-08 14:06:35 +0530

    Update test_utils_quiz2.py

--- tests/test_utils_quiz2.py
+++ tests/test_utils_quiz2.py
@@ -8,7 +8,7 @@ def test_create_split():
     X = iris.data[:n]
     y = iris.target[:n]
     X_train, y_train, X_val, y_val, X_test, y_test = create_split(X, y, 0.7, 0.1)
-    assert len(X_train)+1 == int(0.7 * len(X))
+    assert len(X_train) == int(0.7 * len(X))
     assert len(X_test) == math.ceil(0.2 * len(X))
-    assert len(X_val)-1 == math.ceil(0.1 * len(X))
+    assert len(X_val) == math.ceil(0.1 * len(X))
     assert len(X_train)+len(X_test)+len(X_val) == n
