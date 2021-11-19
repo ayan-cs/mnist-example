bb1961c62c15c7389d976528fae9faff37f3110b
ayan-cs <ayanabhag@gmail.com>
2021-11-19 13:20:23 +0530

    quiz 3

--- quiz3/utils.py
+++ quiz3/utils.py
@@ -9,10 +9,10 @@ def svm_load_predict(test_X, test_y):
     svm = pickle.load(open(best_svm, 'rb'))
     y_pred = svm.predict(test_X)
     acc = accuracy_score([test_y], y_pred)
-    return y_pred, 0.8
+    return y_pred, acc
 
 def dtree_load_predict(test_X, test_y):
     dtree = pickle.load(open(best_dtree, 'rb'))
     y_pred = dtree.predict(test_X)
     acc = accuracy_score([test_y], y_pred)
-    return y_pred, 0.8
+    return y_pred, acc
